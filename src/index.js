c5385d035c9590ca4ba970be0493edaabb89d449
nupaT <trash_jack@mail.ru>
2020-09-30 23:16:47 +0300

    init: array-problem tast. add: min function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (arrayMin) {
+  let currMinVal = 0;      
+  let minResult = 0;
+
+  if(arrayMin === 0 || arrayMin === undefined) {
+    minResult = 0;
+  } else {
+    for (let currMinVal of arrayMin ) {
+      if(minResult > currMinVal){
+        minResult = currMinVal;
+      }
+    }
+  }
+
+  return minResult;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (arrayMax) {
+let currMaxVal = 0;
+let maxResult = 0;
+
+  if(arrayMax === 0 || arrayMax === undefined) {
+    maxResult = 0;
+  } else {
+  for(let currMaxVal of arrayMax){
+    if(maxResult < currMaxVal){
+      maxResult = currMaxVal;
+    }
+  }
+
+  return maxResult;
 }
 
 exports.avg = function avg (array) {
   return 0;
 }
+
+
